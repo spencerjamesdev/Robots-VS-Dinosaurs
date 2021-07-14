e3218aecad76ae747d9f4a17d7f8b724177d4771
spencerjamesdev <spencer.franke@gmail.com>
2021-07-14 13:49:08 -0500

    Added objects 'name' and 'attack power' to weapon class

---
+++ Robot Class.py
@@ -0,0 +1,6 @@
+#Robot needs to have a name and health
+
+class Robot_Class:
+    def __init__(self):
+        self.robot_name = " "
+        self.robot_health = 100
---
+++ Weapon Class.py
@@ -0,0 +1,7 @@
+#Weapon needs to have a name (or type like ex. sword, gun) and weapon
+#needs an attack power
+
+class Weapon_Class:
+    def __init__(self):
+        self.weapon_name = " "
+        self.attack_power = 10
