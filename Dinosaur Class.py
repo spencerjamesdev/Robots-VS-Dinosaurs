56d04172a77b584b70764b08017904f73aa557a2
spencerjamesdev <spencer.franke@gmail.com>
2021-07-14 13:53:36 -0500

    Added objects 'name', 'health', 'attack power' to dino class

---
+++ Dinosaur Class.py
@@ -0,0 +1,8 @@
+#dinosaurs need an object for name, health and attack power
+
+class Dinosaur_Class:
+    def __init__(self):
+        self.dino_name = " "
+        self.dino_health = 100
+        self.dino_attack_power = 15
+        
