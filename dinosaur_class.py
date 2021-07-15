c0815bc5e03f2f2ea2f18bb97b482d4daac3671d
spencerjamesdev <spencer.franke@gmail.com>
2021-07-14 22:08:03 -0500

    instantiated 3 dinosaur objects

--- dinosaur_class.py
+++ dinosaur_class.py
@@ -1,7 +1,8 @@
 #dinosaurs need an object for name, health and attack power
 
 class Dinosaur_Class:
-    def __init__(self, dino_name, dino_health, dino_attack_power):
+    def __init__(self, dino_name, dino_attack_power, dino_health):
         self.dino_name = " "
-        self.dino_health = 0
         self.dino_attack_power = 0
+        self.dino_health = 0
+        
---
+++ herd_class.py
@@ -0,0 +1,16 @@
+#this class is going to create a list of dinosaurs
+from dinosaur_class import Dinosaur
+
+#name: 'rex'  attack power: 20   health: 100
+dino_1 = Dinosaur("Rex", 20, 100)
+dino_2 = Dinosaur("Bran", 15, 100)
+dino_3 = Dinosaur("Tara", 35, 100)
+
+class Herd:
+    def __init__(self):
+        self.dino_list = []
+
+
+    def create_herd(self):
+        self.dino_list =[dino_1 = ]
+
