6fd8a59fbf77615b9b0bd59a35b2d6abf95504b1
spencerjamesdev <spencer.franke@gmail.com>
2021-07-15 20:53:32 -0500

    Added Dinosaur attack method and robot attack Method

--- dinosaur_class.py
+++ dinosaur_class.py
@@ -1,8 +1,11 @@
 #dinosaurs need an object for name, health and attack power
 
 class Dinosaur:
-    def __init__(self, dino_name, dino_attack_power, dino_health):
+    def __init__(self, dino_name, dino_attack_power):
         self.dino_name = " "
         self.dino_attack_power = 0
-        self.dino_health = 0
-        
+        self.dino_health = 100
+
+
+    def dino_attack(self, robot):
+        robot.robot_health - self.dino_attack_power
--- robot_class.py
+++ robot_class.py
@@ -8,6 +8,10 @@ class Robot:
         self.robot_health = 100
         self.Weapon("Ray-Gun", 20)
 
+
+    def robot_attack(self, dinosaur):
+        dinosaur.dino_health - self.Weapon.weapon_attack_power
+
     
     
 
